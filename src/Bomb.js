a72e82465f95547e67710d726063e6349b94ee65
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-04 00:42:56 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -4,10 +4,13 @@ import React from 'react';
 class Bomb  extends  React.Component{
   constructor(props){
     super(props);
-    this.state{
+    this.state({
       secondsLeft: 0
-    }
+    })
   }
 
-  render(`${this.state.secondsLeft} seconds left before I go boom!`);
+  render(){
+    if (this.state.secondsLeft === 0){ return ( `Boom!`); }
+    return ( `${this.state.secondsLeft} seconds left before I go boom!`);
+  }
 }
